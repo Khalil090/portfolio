f3e8a50ad4bc0e30832de5fd48d4e738972ee66a
Ahmed A.Khalil <ahmad.khaliel@gmail.com>
2025-05-14 17:22:10 +0400

    portfoliokh.js

--- portfoliokh.js
+++ portfoliokh.js
@@ -1,9 +1,10 @@
 document.addEventListener('DOMContentLoaded', function () {
     anime({
         targets: '#square',
-        rotate: 90,
-  loop: true,
-  ease: 'inOutExpo',
+        rotate: 90 * 2,
+        duration: 2000,
+        loop: true,
+        ease: 'inOutExpo',
     });
 });
 
